a65a2c8ac4528ae082f057452c31636e396dd8f7
danylop <danylo.pavlichenko@sectigo.com>
2024-02-29 22:58:20 +0200

    Removed usage of transactions because it is not necessary to monitor the integrity of the ID

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,8 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
 import { UserEntity } from "../entities/user.model";
 import { UserDto } from "../dto/user.dto";
-import { Sequelize } from "sequelize-typescript";
 import { RolesService } from "../../roles/services/roles.service";
 
 @Injectable()
@@ -10,24 +9,16 @@ export class UsersService {
   constructor(
     @InjectModel(UserEntity) private userRepository: typeof UserEntity,
     private roleService: RolesService,
-    private sequelize: Sequelize,
   ) {}
 
   async createUser(userDto: UserDto): Promise<UserEntity> {
-    return this.sequelize.transaction(async (transaction) => {
-      try {
-        const  user = await this.userRepository.create(userDto, {transaction});
-        const role = await this.roleService.getRoleByValue('USER');
-        await user.$set('roles', [role.id], { transaction });
-        await transaction.commit();
-        return user;
-      } catch (e) {
-        await transaction.rollback();
-        console.log(e);
-        throw new Error("Error creating user!");
-      }
-    })
-
+    const  user = await this.userRepository.create(userDto);
+    const role = await this.roleService.getRoleByValue('USER');
+    await user.$set('roles', [role.id]);
+    return user;
+  } catch (e) {
+    console.log(e);
+    throw new Error("Error creating user!");
   }
 
   async getAllUsers(): Promise<UserEntity[]> {
